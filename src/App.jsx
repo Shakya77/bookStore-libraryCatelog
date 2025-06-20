6366cf1a0adb3171e7999ff1b1b9d253d67b3bd9
shakya77 <bijanshakya145@gmail.com>
2025-06-20 21:06:45 +0545

    made layouts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import Login from "./views/guest/Login";
 import NotFoundPage from "./views/404";
 import Welcome from "./views/Welcome";
+import GuestLayout from "./views/layouts/guest/GuestLayout";
 
 function App() {
   const status = useSelector((state) => state.auth.isLoggedIn);
@@ -10,12 +11,18 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route element={<GuestLayout />}>
+          <Route path="/" element={<Welcome />} />
+        </Route>
         {
           status ? (
             <Route path="/dashboard" element={<>Logged in vayo hai ta</>} />
           ) : (
-            <Route path="/login" element={<Login />} />
+            <Route>
+              <Route path="/login" element={<Login />} />
+              <Route element={<GuestLayout />}>
+              </Route>
+            </Route>
           )
         }
         <Route path="*" element={<NotFoundPage />} />
--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
+
 export default function Welcome() {
+    useEffect(() => {
+        document.title = "Welcome";
+    }, []);
+
     return (
         <div>
 
-            
         </div>
     )
 }
